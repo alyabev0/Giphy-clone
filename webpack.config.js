daa845368c6150e6f503f268ad8fcfcd8fff1a9c
Pavel <alyabev1998@gmail.com>
2022-03-06 01:52:47 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, "./src/index/index.html")
+        })
+    ],
+    devServer: {
+        port: 8000,
+        historyApiFallback: true,
+        hot: true,
+    }
+
+
+
+}
