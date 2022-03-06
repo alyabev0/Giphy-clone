65d4f942d38d7033f77a3e40030e165382c64e11
Pavel <alyabev1998@gmail.com>
2022-03-06 23:52:20 +0300

    pagination and search are working

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import "./styles/main.scss"
 import $ from "jquery"
 
 console.log("index.js is working")
-var first = document.getElementById("1");
-console.log(first)
+
+var first = ""
 
 function getData(){
-var first = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=AM9Uhu5x5a9UXJUcGq8afp9ukWCApq8x&limit=30");
+
+    $(".inner").empty()
+
+    var input = $("#1").val()
+var first = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"+&api_key=AM9Uhu5x5a9UXJUcGq8afp9ukWCApq8x&limit=32");
 first.done(function(response){
     console.log("success got data", response);
 
@@ -16,9 +20,10 @@ var jiffs = response.data
 var i = "";
 for ( i in jiffs)
 {
-    $('.inner').append("<img src='"+jiffs[i].images.original.url+"' style ='height:350px; width:350px;'/>")
+    $('.inner').append("<img src='"+jiffs[i].images.original.url+"' style ='height:350px; width:350px; margin-top: 0px;'/>")
 }
 })};
 window.getData = getData
 window.check = () => {console.log("this func is working")};
 // window.getData = getData
+var btn = $("#2");
