86f330c2f690972ff1b7dddcf8ecb2796e7d6c1f
Pavel <alyabev1998@gmail.com>
2022-03-06 18:14:35 +0300

    images and  fonts are working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import "./styles/main.scss"
-// import "./assets/images/1532440298_3.jpg"
+import "./scripts/main"
+
+
+// import $ from "jquery"
+
+// console.log("hui");
+// var first = document.getElementById("1");
+// console.log(first)
+
+
+// var first = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=AM9Uhu5x5a9UXJUcGq8afp9ukWCApq8x&limit=30");
+// first.done(function(response){
+//     console.log("success got data", response);
+
+
+// var jiffs = response.data
+// var i = "";
+// for ( i in jiffs)
+// {
+//     $('.inner').append("<img src='"+jiffs[i].images.original.url+"' style ='height:350px; width:350px;'/>")
+// }
+// });
