deff7f31e8c3621afeace000f28977ac6eebc64f
Pavel <alyabev1998@gmail.com>
2022-03-18 22:25:31 +0300

    added mobile version

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ import "./scripts/main"
 
 console.log("index.js is working")
 
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+window.openNav = openNav; 
+  /* Установите ширину боковой панели на 0, а левое поле содержимого страницы - на 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+  window.closeNav = closeNav
